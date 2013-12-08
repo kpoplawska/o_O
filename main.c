462d84ef8d6a8f9525fc05695ab3f8e70ad6ea75
kpoplawska <kpoplawska@sigma.ug.edu.pl>
2013-12-08 19:01:08 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+o_O
+===
+
+#include <stdio.h>
+int main(){
+int x=0,suma=0;
+
+while(x<=20){
+    suma=suma*x;
+    x++;
+          printf("Suma to %d", suma);
+}
+    
+    getch();
+return 0;
+}
